ed23bfa72bb1added0830372359c72b136b9edf0
Michael Philip <mphils55@gmail.com>
2016-04-17 17:05:06 -0400

    Update state

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -1,15 +1,6 @@
 ﻿// Enumeration of the state of the game (items on the todo-list)
-// TODO: Use the actual names for the states
 public enum State {
     Start,
-<<<<<<< HEAD
-    PlaneTickets,
-    Vacuum,
-    Snack,
-	Closet,
-	Stove,
-	ShitHitsTheFan
-=======
     PlaneTicket1,
     PlaneTicket2,
     PlaneTicket3,
@@ -18,5 +9,4 @@ public enum State {
     Closet,
     Stove,
     Finale
->>>>>>> 3923210ea47c072d79167af510a4bd94c54e8e03
 }
