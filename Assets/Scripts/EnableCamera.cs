d4bcfebb5c21a96aa7982fa6922980821b64256f
Greg <gregoryrubenstein@gmail.com>
2016-04-20 12:37:54 -0400

    Visual updates and merged code

---
+++ Assets/Scripts/EnableCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableCamera : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        print(Camera.main);
+        print(Camera.allCameras.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
