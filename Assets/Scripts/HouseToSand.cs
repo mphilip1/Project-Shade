d0c31e67f5e041c3f82f8ed19f176576c8654997
Greg <gregoryrubenstein@gmail.com>
2016-04-28 16:03:56 -0400

    Press R to cause the house to collapse

---
+++ Assets/Scripts/HouseToSand.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseToSand : MonoBehaviour {
+
+    public GameObject container;
+    private Collider[] allColliders;
+    public GameObject terrain;
+
+	// Use this for initialization
+	void Start () {
+        allColliders = container.GetComponentsInChildren<Collider>();
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(enableAndWait(2));
+            
+        }
+	}
+
+    IEnumerator disableObjectsAfterSeconds(GameObject obj, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        obj.SetActive(false);
+    }
+
+    /* Give the terrain time to load so when the house falls, it doesn't lag through it */
+    IEnumerator enableAndWait(float seconds)
+    {
+        terrain.SetActive(true);
+        yield return new WaitForSeconds(seconds);
+        fallDown();
+    }
+
+    public void fallDown()
+    {
+        // Disable colliders to allow house to fall through floor
+        for (int i = 0; i < allColliders.Length; i++)
+        {
+            allColliders[i].enabled = false;
+        }
+
+        // Enable rigidbody to apply gravity
+        container.GetComponent<Rigidbody>().isKinematic = false;
+        StartCoroutine(disableObjectsAfterSeconds(container, 1.5f));
+    }
+}
